3a41a4e062be7bb0d3c7558ae95c447c51897f9b
Davide Borsatto <davide.borsatto@gmail.com>
2021-11-26 14:25:20 +0100

    feat(validator): Add support for Symfony validator with enum lists (#6)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,14 +1,18 @@
 # Changelog
 
+## 2.2
+
+* [**Added**] New `EnumListContraint` and `EnumListValidator` to work with Symfony's validator.
+
 ## 2.1
 
 * [**Added**] New `SmartEnumExceptionInterface` allows you to override the default `fromValue` and `fromValues` methods and throw a custom exception, as long as it implements this interface.
 
 ## 2.0
 
-* [**Added**] New `AbstractEnumJsonListType` and `AbstractEnumSimpleListType` Doctrine types to store data as JSON or comma-separated strings
-* [**Removed**] Dropped support for PHP 7.3, minimum requirement is now 7.4
+* [**Added**] New `AbstractEnumJsonListType` and `AbstractEnumSimpleListType` Doctrine types to store data as JSON or comma-separated strings.
+* [**Removed**] Dropped support for PHP 7.3, minimum requirement is now 7.4.
 
 ## 1.1
 
-* [**Added**] New `EnumConstraint` and `EnumValidator` to work with Symfony's validator
+* [**Added**] New `EnumConstraint` and `EnumValidator` to work with Symfony's validator.
---
+++ src/Bridge/Symfony/Validator/EnumListValidator.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DBorsatto\SmartEnums\Bridge\Symfony\Validator;
+
+use DBorsatto\SmartEnums\EnumFactory;
+use DBorsatto\SmartEnums\Exception\SmartEnumExceptionInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\ConstraintDefinitionException;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+use Symfony\Component\Validator\Exception\UnexpectedValueException;
+use function array_values;
+use function implode;
+use function is_array;
+use function is_string;
+
+class EnumListValidator extends ConstraintValidator
+{
+    public function validate($value, Constraint $constraint): void
+    {
+        if (!$constraint instanceof EnumListConstraint) {
+            throw new UnexpectedTypeException($constraint, EnumListConstraint::class);
+        }
+
+        if (!is_array($value)) {
+            throw new UnexpectedValueException($value, 'string[]');
+        }
+
+        foreach ($value as $item) {
+            if (!is_string($item)) {
+                throw new UnexpectedValueException($value, 'string[]');
+            }
+        }
+
+        /** @var list<string> $value */
+        $value = array_values($value);
+
+        try {
+            $factory = new EnumFactory($constraint->enumClass);
+        } catch (SmartEnumExceptionInterface $exception) {
+            throw new ConstraintDefinitionException('Enum class property of constraint is not valid');
+        }
+
+        try {
+            $factory->fromValues($value);
+        } catch (SmartEnumExceptionInterface $exception) {
+            $this->context->buildViolation($constraint->message)
+                ->setParameter('{{ value }}', implode(', ', $value))
+                ->setParameter('{{ enumClass }}', $constraint->enumClass)
+                ->addViolation();
+        }
+    }
+}
